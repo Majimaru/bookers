3fb16cadb5dd63676e166a03f08e35da30ccb777
EC2 Default User <ec2-user@ip-172-31-45-117.ap-northeast-1.compute.internal>
2023-08-10 02:58:25 +0900

    [Break] 理由：就寝、完了タスク：フラッシュメッセージの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,14 +10,29 @@ class BooksController < ApplicationController
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
+      @books = Book.all
       render :index
     end
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book.id)
+    else
+      flash.now[:notice] = "Book was not successfully updated."
+      @book = Book.find(params[:id])
+      render :edit
+    end
   end
   
   def destroy
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,5 @@
 class Book < ApplicationRecord
   
-  validates :title, presence: true
-  validates :body, presence: true
+  validates :title, :body, presence: true
   
 end
